9e7cd82804803d0c0a3670ad4b20a4809d091c81
Louis <louis.lesueur@gmx.fr>
2020-04-11 13:23:10 +0200

    début du livre 1 + python clean

--- python/generate.py
+++ python/generate.py
@@ -35,65 +35,71 @@ class Union:
 					G.add_edge(self.mother.name,kid.name)
 
 
+class graph:
+	def __init__(self, unions):
+		self.Graph=pgv.AGraph(directed=True )
+		self.unions = unions
+		for u in unions:
+			u.gen_tree(self.Graph)
 
-with open('Apollodore', 'r') as file:
-    data = np.loadtxt(file,  dtype='str', comments='#', delimiter=',')
-
-
-persons = [Person(dat[0]) for dat in data]
-unions = [Union(*dat, i) for i,dat in enumerate(data)]
-
-def find_union(person, unions):
-	for u in unions:
-		if person == u.father:
-			return u
+		self.Dict = {}
+		for i,n in enumerate(self.Graph.nodes()):
+			self.Dict[n]=i
 
 
-G=pgv.AGraph(directed=True )
-for u in unions:
-	u.gen_tree(G)
-
-def too_many_ancestors(G):
-	for n in G.nodes():
-		if len(G.predecessors(n)) > 1 and n[0] != 'u':
-			print(n)
-
-def many_unions(G):
-	for n in G.nodes():
-		if len(G.successors(n)) > 1 and n[0] != 'u':
-			print(n)
-
-
-
-
-def to_js(G):
-
-	dG = {}
-	for i,n in enumerate(G.nodes()):
-		dG[n]=i
-
-	file = open('../js/global_tree.js', 'w')
+	def to_js(self):
+		file = open('../js/global_tree.js', 'w')
+		file.write("const nodes = new vis.DataSet([\n")
+		for n in self.Graph.nodes():
+			file.write("    { id: "+str(self.Dict[n])+", label: '"+n+"', color: 'blue', shape: 'dot'},\n")
+		file.write("]);\n")
+		file.write("const edges = new vis.DataSet([\n")
+		for u in self.unions:
+			for child in u.children:
+				if child.name != 'none':
+					if u.father.name != '?':
+						file.write("    { from: "+str(self.Dict[u.father.name])+", to: "+str(self.Dict[child.name])+", relation: 'father', arrows: 'to'},\n")
+					if u.mother.name != '?':
+						file.write("    { from: "+str(self.Dict[u.mother.name])+", to: "+str(self.Dict[child.name])+", relation: 'mom', arrows: 'to'},\n")
+					if u.father.name != '?' and u.mother.name != '?':
+						file.write("    { from: "+str(self.Dict[u.mother.name])+", to: "+str(self.Dict[u.father.name])+", relation: 'partner', color: 'red'},\n")
+		file.write("]);")
+		file.close()
 
 
-	file.write("const nodes = new vis.DataSet([\n")
-	for n in G.nodes():
-			file.write("    { id: "+str(dG[n])+", label: '"+n+"', color: 'blue', shape: 'dot'},\n")
-	file.write("]);\n")
-
-	file.write("const edges = new vis.DataSet([\n")
-	for u in unions:
-		for child in u.children:
-			if child.name != 'none':
-				if u.father.name != '?':
-					file.write("    { from: "+str(dG[u.father.name])+", to: "+str(dG[child.name])+", relation: 'father', arrows: 'to'},\n")
-				if u.mother.name != '?':
-					file.write("    { from: "+str(dG[u.mother.name])+", to: "+str(dG[child.name])+", relation: 'mom', arrows: 'to'},\n")
-				if u.father.name != '?' and u.mother.name != '?':
-					file.write("    { from: "+str(dG[u.mother.name])+", to: "+str(dG[u.father.name])+", relation: 'partner', color: 'red'},\n")
-	file.write("]);")
-
-	file.close()
+	def to_svg(self):
+		self.Graph.draw('../img/out.svg', 'svg', prog='dot')
 
 
-G.draw('../img/out.svg', 'svg', prog='dot')
-to_js(G)
+class Tree:
+	def __init__(self, author):
+		with open(author, 'r') as file:
+			data = np.loadtxt(file,  dtype='str', comments='#', delimiter=',')
+
+		self.unions = [Union(*dat, i) for i,dat in enumerate(data)]
+		self.graph = graph(self.unions)
+
+
+	def find_union(person, unions):
+		for u in unions:
+			if person == u.father:
+				return u
+		return None
+
+
+def extract_for_label(title):
+	file_out = open(title+'_persos', 'w')
+	with open(title, 'r') as file:
+		data = np.loadtxt(file,  dtype='str', comments='#', delimiter=',')
+
+	for d in data:
+		file_out.write(d[0]+'\n')
+		for c in d[2][1:-1].split(" "):
+			file_out.write(c+'\n')
+
+	file_out.close()
+
+
+Apo = Tree('Apollodore')
+Apo.graph.to_js()
+extract_for_label("Apollodore")

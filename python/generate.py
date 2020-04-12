28f0f0ebedbef6c5f844ca236774744db422700d
Louis <louis.lesueur@gmx.fr>
2020-04-12 13:51:29 +0200

    vert

--- python/generate.py
+++ python/generate.py
@@ -3,12 +3,16 @@ import numpy as np
 
 
 class Person:
-	def __init__(self, name,id):
+	def __init__(self, name,id,origin=0):
 		self.name = name
 		self.id = id
+		self.origin=origin
 
 	def to_js(self):
-		return "    { id: "+str(self.id)+", label: '"+self.name+"', color: 'orange', shape: 'dot'},\n"
+		if self.origin==0:
+			return "    { id: "+str(self.id)+", label: '"+self.name+"', color: 'orange', shape: 'dot'},\n"
+		else:
+			return "    { id: "+str(self.id)+", label: '"+self.name+"', color: 'green', shape: 'dot'},\n"
 
 	def __str__(self):
 		return self.name
@@ -18,17 +22,22 @@ class Person:
 
 
 class Relation:
-	def __init__(self, nature, id1, id2):
+	def __init__(self, nature, id1, id2, origin=0):
 		self.nature = nature
 		self.id1 = id1
 		self.id2 = id2
+		self.origin = origin
 
 	def to_js(self):
 		color = "orange"
 		if self.nature == "partner":
 			color = "red"
+			if self.origin != 0:
+				color = "green"
 			return "    { from: "+str(self.id1)+", to: "+str(self.id2)+", relation: '"+self.nature+"', color: '"+color+"'},\n"
 		else:
+			if self.origin != 0:
+				color = "green"
 			return "    { from: "+str(self.id1)+", to: "+str(self.id2)+", relation: '"+self.nature+"', arrows: 'to', color: '"+color+"'},\n"
 
 
@@ -58,19 +67,19 @@ class Tree:
 		self.persons = {}
 		self.relations = []
 
-		def add_person(name):
+		def add_person(name, origin):
 			idx = len(self.names)
 			if name != '?':
 				if name != 'none':
 					self.names.add(name)
 					if len(self.names)>idx:
-						self.persons[name] = (Person(name,idx))
+						self.persons[name] = (Person(name,idx,origin))
 
-		def add_relation(name1, name2, nature):
+		def add_relation(name1, name2, nature,origin=0):
 			if name1 in self.names and name2 in self.names:
 				id1 = self.persons[name1].id
 				id2 = self.persons[name2].id
-				self.relations.append(Relation(nature,id1,id2))
+				self.relations.append(Relation(nature,id1,id2,origin))
 
 
 
@@ -80,14 +89,14 @@ class Tree:
 				for i,dat in enumerate(data):
 					idx = len(self.names)
 
-					add_person(dat[0])
-					add_person(dat[1])
-					add_relation(dat[0],dat[1],"partner")
+					add_person(dat[0],j)
+					add_person(dat[1],j)
+					add_relation(dat[0],dat[1],"partner",j)
 
 					for child in dat[2][1:-1].split(" "):
-						add_person(child)
-						add_relation(dat[0],child,"father")
-						add_relation(dat[1],child,"mother")
+						add_person(child,j)
+						add_relation(dat[0],child,"father",j)
+						add_relation(dat[1],child,"mother",j)
 
 			file.close()
 

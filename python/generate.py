4100ef432f68ac61507904385beefd98a355ae81
Louis <louis.lesueur@gmx.fr>
2020-04-12 13:42:15 +0200

    nouvelle structure plus souple

--- python/generate.py
+++ python/generate.py
@@ -3,8 +3,12 @@ import numpy as np
 
 
 class Person:
-	def __init__(self, name):
+	def __init__(self, name,id):
 		self.name = name
+		self.id = id
+
+	def to_js(self):
+		return "    { id: "+str(self.id)+", label: '"+self.name+"', color: 'orange', shape: 'dot'},\n"
 
 	def __str__(self):
 		return self.name
@@ -13,99 +17,84 @@ class Person:
 		return self.name == pers.name
 
 
-class Union:
-	def __init__(self, father, mother, children, id):
-		self.father = Person(father)
-		self.mother = Person(mother)
-		self.children = [Person(kid) for kid in children[1:-1].split(" ")]
-		self.id = id
+class Relation:
+	def __init__(self, nature, id1, id2):
+		self.nature = nature
+		self.id1 = id1
+		self.id2 = id2
 
-	def gen_tree(self, G):
-		if self.father.name != '?':
-			G.add_node(self.father.name)
-		if self.mother.name != '?':
-			G.add_node(self.mother.name)
+	def to_js(self):
+		color = "orange"
+		if self.nature == "partner":
+			color = "red"
+			return "    { from: "+str(self.id1)+", to: "+str(self.id2)+", relation: '"+self.nature+"', arrows: 'to', color: '"+color+"'},\n"
+		else:
+			return "    { from: "+str(self.id1)+", to: "+str(self.id2)+", relation: '"+self.nature+"', color: '"+color+"'},\n"
 
-		for kid in self.children:
-			if kid.name != 'none':
-				if self.father.name != '?':
-					G.add_edge(self.father.name,kid.name)
-				if self.mother.name != '?':
-					G.add_edge(self.mother.name,kid.name)
 
 
 class graph:
-	def __init__(self, unions):
-		self.Graph=pgv.AGraph(directed=True )
-		self.unions = unions
-		for u in unions:
-			u.gen_tree(self.Graph)
-
-		self.Dict = {}
-		for i,n in enumerate(self.Graph.nodes()):
-			self.Dict[n]=i
+	def __init__(self, persons, relations):
+		self.persons = persons
+		self.relations = relations
 
 
 	def to_js(self):
 		file = open('../js/global_tree.js', 'w')
 		file.write("const nodes = new vis.DataSet([\n")
-		for n in self.Graph.nodes():
-			file.write("    { id: "+str(self.Dict[n])+", label: '"+n+"', color: 'orange', shape: 'dot'},\n")
+		for pers in self.persons:
+			file.write(pers.to_js())
 		file.write("]);\n")
 		file.write("const edges = new vis.DataSet([\n")
-		for u in self.unions:
-			if u.id == 0:
-				color='orange'
-			else:
-				color='green'
-			for child in u.children:
-				if child.name != 'none':
-					if u.father.name != '?':
-						file.write("    { from: "+str(self.Dict[u.father.name])+", to: "+str(self.Dict[child.name])+", relation: 'father', arrows: 'to', color: '"+color+"'},\n")
-					if u.mother.name != '?':
-						file.write("    { from: "+str(self.Dict[u.mother.name])+", to: "+str(self.Dict[child.name])+", relation: 'mom', arrows: 'to', color: '"+color+"'},\n")
-					if u.father.name != '?' and u.mother.name != '?':
-						file.write("    { from: "+str(self.Dict[u.mother.name])+", to: "+str(self.Dict[u.father.name])+", relation: 'partner', color: 'red'},\n")
+		for rel in self.relations:
+			file.write(rel.to_js())
 		file.write("]);")
 		file.close()
 
 
-	def to_svg(self):
-		self.Graph.draw('../img/out.svg', 'svg', prog='dot')
-
-	def extract_for_label(self,title):
-		file_out = open(title+'_persos', 'w')
-		for n in np.sort(self.Graph.nodes()):
-			file_out.write(n+'\n')
-		file_out.close()
-
-	def no_parents(self):
-		for n in self.Graph.nodes():
-			if len(self.Graph.predecessors(n)) == 0:
-				print(n)
-
-
 class Tree:
 	def __init__(self, authors):
-		self.unions = []
+		self.names = set({})
+		self.persons = {}
+		self.relations = []
+
+		def add_person(name):
+			idx = len(self.names)
+			if name != '?':
+				if name != 'none':
+					self.names.add(name)
+					if len(self.names)>idx:
+						self.persons[name] = (Person(name,idx))
+			
+		def add_relation(name1, name2, nature):
+			if name1 in self.names and name2 in self.names:
+				id1 = self.persons[name1].id
+				id2 = self.persons[name2].id
+				self.relations.append(Relation(nature,id1,id2))
+				
+
+
 		for j,a in enumerate(authors):
 			with open(a, 'r') as file:
 				data = np.loadtxt(file,  dtype='str', comments='#', delimiter=',')
 				for i,dat in enumerate(data):
-					self.unions.append(Union(*dat, j))
+					idx = len(self.names)
+
+					add_person(dat[0])
+					add_person(dat[1])
+					add_relation(dat[0],dat[1],"partner")
+		
+					for child in dat[2][1:-1].split(" "):
+						add_person(child)
+						add_relation(dat[0],child,"father")
+						add_relation(dat[1],child,"mother")
+
 			file.close()
+			
 
-		self.graph = graph(self.unions)
+		self.graph = graph(self.persons.values(), self.relations)
 
-
-	def find_union(person, unions):
-		for u in unions:
-			if person == u.father:
-				return u
-		return None
 
 
 Apo = Tree(['Apollodore', 'Ajouts'])
 Apo.graph.to_js()
-Apo.graph.no_parents()
-Apo.graph.extract_for_label("Apollodore")

7a41b2edec0b7c66ca4b690edfa091fa309d9b6b
Louis <louis.lesueur@gmx.fr>
2020-06-13 15:19:24 +0200

    minor corrections

--- js/nav.js
+++ js/nav.js
@@ -28,6 +28,10 @@ function hideOrphans() {
     if (node.color.background == 'orange'){
       if (network.getConnectedNodes(i).length == 0) {
         node.hidden = true;
+        node.x = 0
+        node.y = 0
+        node.fixed = true
+        node.physics = false
         to_hide.push(node)
       }
     }
